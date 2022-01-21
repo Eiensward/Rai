1e3968dc0a1d861eafad3029f49e610e52423428
Bob Smith <daviddchen.7@gmail.com>
2022-01-21 08:19:46 -0800

    Add files via upload

--- LoremIpsumFinishedProduct4Assign2/app.js
+++ LoremIpsumFinishedProduct4Assign2/app.js
@@ -1,15 +1,212 @@
 // lorem text
 const text = [
-  `Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.`,
-  `Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.`,
-  `Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.`,
-  `Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers kitty power ignore the squirrels, you'll never catch them anyway for what a cat-ass-trophy! or purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Pretend you want to go out but then don't bite off human's toes, yet disappear for four days and return home with an expensive injury; bite the vet so catch eat throw up catch eat throw up bad birds. `,
-  `This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them.
-I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Michelle, I don't regret this, but I both rue and lament it. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat.`,
-  `Airedale hard cheese mozzarella. Pecorino melted cheese port-salut emmental babybel cheese and wine melted cheese manchego. Everyone loves blue castello everyone loves fromage cheese slices airedale cheddar cream cheese. Bavarian bergkase who moved my cheese halloumi port-salut gouda jarlsberg ricotta rubber cheese. Stinking bishop smelly cheese brie.`,
-  `Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably haven't heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.`,
-  `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
-  `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
+  `I am the bone of my sword, grades are my veins, and sleep in my boDY. I have created a million essays, unknown to death, nor know to death. Have withstood many hours to forge a thousand documents. Yet those hands shall never hold any money. So as I pray... UNLIMITED STUDENT WORKS!!!! \EPIC PIANO STARTS PLAYING*/ AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH, FULL POWER, OVER 9000 LET'S GOOOO, TO THE MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON`,
+  `SHREK: Example? Okay, um, ogres are like onions. (he holds out his onion)
+
+  DONKEY: (sniffs the onion) They stink?
+  
+  SHREK: Yes - - No!
+  
+  DONKEY: They make you cry?
+  
+  SHREK: No!
+  
+  DONKEY: Oh, you leave them in the sun, they get all brown, start sproutin' little white hairs.
+  
+  SHREK: No! Layers! Onions have layers. Ogres have layers! Onions have layers. You get it? We both have layers. (he throws away the onion and walks off)`,
+  `Somebody once told me the world is gonna roll me
+  I ain't the sharpest tool in the shed
+  She was looking kind of dumb with her finger and her thumb
+  In the shape of an "L" on her forehead
+  Well the years start coming and they don't stop coming
+  Fed to the rules and I hit the ground running
+  Didn't make sense not to live for fun
+  Your brain gets smart but your head gets dumb
+  So much to do, so much to see
+  So what's wrong with taking the back streets?
+  You'll never know if you don't go
+  You'll never shine if you don't glow
+  Hey now, you're an all-star, get your game on, go play
+  Hey now, you're a rock star, get the show on, get paid
+  And all that glitters is gold
+  Only shooting stars break the mold
+  It's a cool place and they say it gets colder
+  You're bundled up now, wait 'til you get older
+  But the meteor men beg to differ
+  Judging by the hole in the satellite picture
+  The ice we skate is getting pretty thin
+  The water's getting warm so you might as well swim
+  My world's on fire, how about yours?
+  That's the way I like it and I'll never get bored
+  Hey now, you're an all-star, get your game on, go play
+  Hey now, you're a rock star, get the show on, get paid
+  All that glitters is gold
+  Only shooting stars break the mold
+  Hey now, you're an all-star, get your game on, go play
+  Hey now, you're a rock star, get the show, on get paid
+  And all that glitters is gold
+  Only shooting stars
+  Somebody once asked could I spare some change for gas?
+  I need to get myself away from this place
+  I said, "Yup" what a concept
+  I could use a little fuel myself
+  And we could all use a little change
+  Well, the years start coming and they don't stop coming
+  Fed to the rules and I hit the ground running
+  Didn't make sense not to live for fun
+  Your brain gets smart but your head gets dumb
+  So much to do, so much to see
+  So what's wrong with taking the back streets?
+  You'll never know if you don't go (go!)
+  You'll never shine if you don't glow
+  Hey now, you're an all-star, get your game on, go play
+  Hey now, you're a rock star, get the show on, get paid
+  And all that glitters is gold
+  Only shooting stars break the mold
+  And all that glitters is gold
+  Only shooting stars break the mold`,
+  `To be, or not to be, that is the question:
+  Whether 'tis nobler in the mind to suffer
+  The slings and arrows of outrageous fortune,
+  Or to take arms against a sea of troubles
+  And by opposing end them. To die—to sleep,
+  No more; and by a sleep to say we end
+  The heart-ache and the thousand natural shocks
+  That flesh is heir to: 'tis a consummation
+  Devoutly to be wish'd. To die, to sleep;
+  To sleep, perchance to dream—ay, there's the rub:
+  For in that sleep of death what dreams may come,
+  When we have shuffled off this mortal coil,
+  Must give us pause—there's the respect
+  That makes calamity of so long life.
+  For who would bear the whips and scorns of time,
+  Th'oppressor's wrong, the proud man's contumely,
+  The pangs of dispriz'd love, the law's delay,
+  The insolence of office, and the spurns
+  That patient merit of th'unworthy takes,
+  When he himself might his quietus make
+  With a bare bodkin? Who would fardels bear,
+  To grunt and sweat under a weary life,
+  But that the dread of something after death,
+  The undiscovere'd country, from whose bourn
+  No traveller returns, puzzles the will,
+  And makes us rather bear those ills we have
+  Than fly to others that we know not of?
+  Thus conscience doth make cowards of us all,
+  And thus the native hue of resolution
+  Is sicklied o'er with the pale cast of thought,
+  And enterprises of great pith and moment
+  With this regard their currents turn awry
+  And lose the name of action.`,
+  `JOHN CENAAAAAAA JOHN CENAAAAAAAJOHN CENAAAAAAA JOHN CENAAAAAAA JOHN CENAAAAAAAJOHN CENAAAAAAA JOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAAJOHN CENAAAAAAA`,
+  `We're no strangers to love
+  You know the rules and so do I
+  A full commitment's what I'm thinking of
+  You wouldn't get this from any other guy
+  I just wanna tell you how I'm feeling
+  Gotta make you understand
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  We've known each other for so long
+  Your heart's been aching but you're too shy to say it
+  Inside we both know what's been going on
+  We know the game and we're gonna play it
+  And if you ask me how I'm feeling
+  Don't tell me you're too blind to see
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give, never gonna give
+  (Give you up)
+  We've known each other for so long
+  Your heart's been aching but you're too shy to say it
+  Inside we both know what's been going on
+  We know the game and we're gonna play it
+  I just wanna tell you how I'm feeling
+  Gotta make you understand
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye`,
+  `Astro-naut
+  What you know about rollin' down in the deep?
+  When your brain goes numb, you can call that mental freeze
+  When these people talk too much, put that shit in slow motion, yeah
+  I feel like an astronaut in the ocean, ayy
+  What you know about rollin' down in the deep?
+  When your brain goes numb, you can call that mental freeze
+  When these people talk too much, put that shit in slow motion, yeah
+  I feel like an astronaut in the ocean
+  She say that I'm cool (damn straight)
+  I'm like, "Yeah, that's true" (that's true)
+  I believe in G-O-D (ayy)
+  Don't believe in T-H-O-T
+  She keep playing me dumb (play me)
+  I'ma play her for fun (uh-huh)
+  Y'all don't really know my mental
+  Lemme give you the picture like stencil
+  Falling out, in a drought
+  No flow, rain wasn't pouring down (pouring down)
+  See, that pain was all around
+  See, my mode was kinda lounged
+  Didn't know which-which way to turn
+  Flow was cool but I still felt burnt
+  Energy up, you can feel my surge
+  I'ma kill everything like this purge (ayy)
+  Let's just get this straight for a second, I'ma work
+  Even if I don't get paid for progression, I'ma get it (get it)
+  Everything that I do is electric
+  I'ma keep it in a motion, keep it moving like kinetic, ayy (yeah, yeah, yeah, yeah)
+  Put this shit in a frame, better know I don't blame
+  Everything that I say, man, I seen you deflate
+  Let me elevate, this ain't a prank
+  Have you walkin' on a plank, la-la-la-la-la, like
+  Both hands together, God, let me pray (now let me pray)
+  Uh, I've been going right, right around, call that relay (Masked Wolf)
+  Pass the baton, back to them all
+  Swimming in the pool, Kendrick Lamar, uh
+  Want a piece of this, a piece of mine, my peace a sign
+  Can you please read between the lines?
+  My rhyme's inclined to break your spine
+  They say that I'm so fine
+  You could never match my grind (true)
+  Please do not, not waste my time (Wolf)
+  What you know about rollin' down in the deep?
+  When your brain goes numb, you can call that mental freeze
+  When these people talk too much, put that shit in slow motion, yeah
+  I feel like an astronaut in the ocean, ayy
+  What you know about rollin' down in the deep?
+  When your brain goes numb, you can call that mental freeze
+  When these people talk too much, put that shit in slow motion, yeah
+  I feel like an astronaut in the ocean`,
+  `YOU EXPECTED GIVVERISH BUT IT WAS ME, DIO, Za WARUDO!!! Time Stop. Huhuhu, did you really think you could beat me? Or excape ME? Fool, only buffoons think like such!!! Wahahahahahahaahahaha, you shall never WIN!!! DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO DIO`,
+  `Mr Rai, how ya doing, im doing good ya, shreking on the shrek cause im shrekso on the shreksophone, woooooooo. Mr. Rai? More like Mr RAAAAAAAAAAAAAAY. SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK SHREK`,
 ];
 
 const form = document.querySelector(".lorem-form");
@@ -37,3 +234,5 @@ else{
 }
 
 });
+
+
